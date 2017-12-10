6214d554798e48bc7adf97c0a414a7c02c78f117
wilversings <aiordachioaei.marius@gmail.com>
2017-12-10 15:53:55 +0200

    .

--- parsing/domain/state_machine.py
+++ parsing/domain/state_machine.py
@@ -3,8 +3,6 @@ from parsing.domain.state import State
 
 class StateMachine:
 
-
-
     def __init__(self, start: State):
         
         self.__start = start
@@ -12,25 +10,27 @@ class StateMachine:
             "node": None,
             "neighbours": []
         }
-        self.build_state_machine(start, self.start_root, set())
+        state_set = {start}
+        self.build_state_machine(start, self.start_root, state_set)
+
+        for x in state_set:
+            print (x)
+            print()
 
     def build_state_machine(self, current: State, edge: dict, state_set):
 
         neighbours = []
         for sym in (x.next for x in current.items if x.has_next):
 
-            print(current)
-            print()
-            #print("Ma duc cu symbolul: " + str(sym))
             next = current.go_to(sym)
             if next in state_set:
                 neighbours.append(next)
                 continue
             state_set.add(next)
             if next.is_valid:
-                neighbours.append(self.build_state_machine(next, {
+                neighbours.append((self.build_state_machine(next, {
                     "node": next
-                }, state_set))
+                }, state_set), sym))
 
         edge['neighbours']= neighbours
 
